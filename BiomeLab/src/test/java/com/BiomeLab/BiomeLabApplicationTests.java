66e08d3fc81f75d0adde129fa2146ed46fee0aca
Eduardo-Locaspi <dudulocaspi@gmail.com>
2026-05-29 18:04:44 -0300

    Arquitetura basica Spring Boot

---
+++ BiomeLab/src/test/java/com/BiomeLab/BiomeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BiomeLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiomeLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
