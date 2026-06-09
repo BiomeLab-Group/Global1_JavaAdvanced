7228b1727690cdb0a9cdde6f9ba38d28f1296a52
Eduardo-Locaspi <dudulocaspi@gmail.com>
2026-06-09 18:31:23 -0300

    L maldito

--- BiomeLab/src/main/java/com/BiomeLab/Security/JWTUtil.java
+++ BiomeLab/src/main/java/com/BiomeLab/Security/JWTUtil.java
@@ -1,59 +1,50 @@
 package com.BiomeLab.Security;
-
+ 
 import java.util.Date;
-
+ 
 import javax.crypto.SecretKey;
-
+ 
 import org.springframework.stereotype.Component;
-
+ 
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-
+ 
 @Component
 public class JWTUtil {
-	
-	//chave pública
-	private final SecretKey CHAVE = Jwts.SIG.HS256.key().build();
-	
-	//geração de token
-	public String gerarToken(String username, Integer duracao) {
-		
-		Date data_atual = new Date();
-		
-		JwtBuilder builder = Jwts.builder()
-								 .subject(username)
-								 .issuedAt(data_atual)
-//								 .expiration(
-//								  new Date(data_atual.getTime() + (1000 * 60 * 60 * duracao)))
-								 .signWith(CHAVE);
-								 
-		return builder.compact();
-	}
-	
-	//extrair um usuário de um token
-	public String extrairUsername(String token) {
-		
-		try {
-			JwtParser parser = Jwts.parser().verifyWith(CHAVE).build();
-			
-			return parser.parseSignedClaims(token).getPayload().getSubject();
-		} catch(Exception e) {
-			return null;
-		}
-		
-	}
-	
-	//validar token (com base na chave pública)
-	public boolean validarToken(String token) {
-		try {
-			JwtParser parser = Jwts.parser().verifyWith(CHAVE).build();
-			parser.parseSignedClaims(token);
-			return true;
-		} catch(Exception e) {
-			return false;
-		}
-	}
-	
-	
+ 
+    // chave secreta para assinar o token
+    private final SecretKey CHAVE = Jwts.SIG.HS256.key().build();
+ 
+    // gera token com expiração de 30 dias
+    public String gerarToken(String username, Integer duracao) {
+        Date data_atual = new Date();
+        JwtBuilder builder = Jwts.builder()
+                .subject(username)
+                .issuedAt(data_atual)
+                .expiration(new Date(data_atual.getTime() + (1000L * 60 * 60 * 24 * 30))) // 30 dias
+                .signWith(CHAVE);
+        return builder.compact();
+    }
+ 
+    // extrai o usuário do token
+    public String extrairUsername(String token) {
+        try {
+            JwtParser parser = Jwts.parser().verifyWith(CHAVE).build();
+            return parser.parseSignedClaims(token).getPayload().getSubject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+ 
+    // valida o token com base na chave
+    public boolean validarToken(String token) {
+        try {
+            JwtParser parser = Jwts.parser().verifyWith(CHAVE).build();
+            parser.parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
